3acd19b359e6aec9dc622cb75e7787c13441b4bd
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2023-12-08 17:51:40 +0530

    removing extra content

--- mywebsite/homepage/models.py
+++ mywebsite/homepage/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Person_Detail:
-    full_name = str
-    aim = str
-    introduction = str
-
-class Education_Detail:
-    college_details = str
-    senior_secondary_detail = str
-    metriculation_detail = str
 
 class Project_Detail:
     name = str
@@ -20,15 +11,3 @@ class Project_Detail:
     category = str
     link = str
 
-class Service_Detail:
-    service_name = str
-    service_detail = str
-
-class Contact_Detail:
-    contact_number = str
-    whatsapp_number = str
-    email = str
-    linkedin_link = str
-    instagram_link =str
-    facebook_link = str
-    twitter_link = str
--- mywebsite/homepage/urls.py
+++ mywebsite/homepage/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='index.html'),
-    # path('contactForm',views.contacted,name='contact')
-    # path('add',views.add, name='add'),
-    # path('multiply',views.multiply,name='multiply'),
-    # path('divide',views.divide,name='divide'),
-    # path('substract',views.substract,name='substract')
 ]
