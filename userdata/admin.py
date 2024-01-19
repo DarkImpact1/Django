b455855480e2578d004e4f6f560761a374ebad00
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-01-19 13:07:00 +0530

    registering model to admin panel

--- userdata/admin.py
+++ userdata/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Userdetail,Projectdetail,Educationdetail
+from .models import Userdetail,Projectdetail,Educationdetail,WorkDetail,Servicedetails,Contactdetail
 # Register your models here.
 admin.register(Userdetail)
+admin.register(Projectdetail,Educationdetail,WorkDetail,Servicedetails,Contactdetail)
